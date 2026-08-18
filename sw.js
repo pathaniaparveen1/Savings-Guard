4f710b442e56f3a9d5ccb037e301f0e68fdeaff1
pathaniaparveen1 <pathania.parveen1@gmail.com>
2026-08-18 16:08:20 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='saving-guard-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
+const CACHE='saving-guard-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png','./msal-browser.min.js'];
 
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
